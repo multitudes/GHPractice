7071a397174f3d9bac8b4080eb21f91b29f8b70f
laurent <wrmultitudes@gmail.com>
2020-05-10 20:50:50 +0200

    first URLrequest

---
+++ SeanAllenGHPractice/Manager/NetworkManager.swift
@@ -0,0 +1,54 @@
+//
+//  NetworkManager.swift
+//  SeanAllenGHPractice
+//
+//  Created by Laurent B on 10/05/2020.
+//  Copyright © 2020 Laurent B. All rights reserved.
+//
+
+import Foundation
+
+
+class NetworkManager {
+    
+    static let shared = NetworkManager()
+    let baseURL = "https://api.github.com/users/"
+    
+    private init() {}
+    
+    func getFollowers(for username: String, page: Int, completed: @escaping ([Follower]?, String?) -> Void ) {
+        let endpoint = baseURL + "\(username)/followers?per_page=100&page=\(page)"
+        
+        guard let url = URL(string: endpoint) else {
+            completed(nil, "This username created an invalid request")
+            return
+        }
+        
+        let task = URLSession.shared.dataTask(with: url) { data, response, error in
+            
+            if let _ = error {
+                completed(nil, "Unable to complete, check your internet connection")
+                return
+            }
+            guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
+                completed(nil, "Invalid response from the server, pls try again")
+                return
+            }
+            
+            guard let data = data else {
+                completed(nil, "The data received was invalid, pls try again")
+                return
+            }
+            
+            do {
+                let decoder = JSONDecoder()
+                decoder.keyDecodingStrategy = .convertFromSnakeCase
+                let followers = try decoder.decode([Follower].self, from: data)
+                completed(followers, nil)
+            } catch {
+                completed(nil, "The data received from the server was invalid")
+            }
+        }
+        task.resume()
+    }
+}
--- SeanAllenGHPractice/Screens/FollowerListVC.swift
+++ SeanAllenGHPractice/Screens/FollowerListVC.swift
@@ -15,11 +15,23 @@ class FollowerListVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
-        navigationController?.isNavigationBarHidden = false
         navigationController?.navigationBar.prefersLargeTitles = true
+
+        NetworkManager.shared.getFollowers(for: username, page: 1) { (followers, errorMessage) in
+            guard let followers = followers else {
+                self.presentGFAlertOnMainThread(title: "Bad Stuff happened", message: errorMessage ?? "", buttonTitle: "OK")
+                return
+            }
+            print(followers.count)
+            print(followers)
+            
+        }
     }
     
-
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.setNavigationBarHidden(false, animated: true)
+    }
     
 
 
