512b32cf4043f7f4f59ad6dc0ff8650ede662a38
laurent <wrmultitudes@gmail.com>
2020-05-11 12:06:03 +0200

    refactor with result - time to start on collectionView Cell

--- SeanAllenGHPractice/Screens/FollowerListVC.swift
+++ SeanAllenGHPractice/Screens/FollowerListVC.swift
@@ -17,22 +17,20 @@ class FollowerListVC: UIViewController {
         view.backgroundColor = .systemBackground
         navigationController?.navigationBar.prefersLargeTitles = true
 
-        NetworkManager.shared.getFollowers(for: username, page: 1) { (followers, errorMessage) in
-            guard let followers = followers else {
-                self.presentGFAlertOnMainThread(title: "Bad Stuff happened", message: errorMessage!.rawValue, buttonTitle: "OK")
-                return
-            }
-            print(followers.count)
-            print(followers)
+        NetworkManager.shared.getFollowers(for: username, page: 1) { result in
             
+            switch result {
+                case .success(let followers):
+                    print(followers)
+                case .failure(let error):
+                    self.presentGFAlertOnMainThread(title: "Bad Stuff happened", message: error.rawValue, buttonTitle: "OK")
+            }
         }
     }
+    
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         navigationController?.setNavigationBarHidden(false, animated: true)
     }
-    
-
-
 }
--- SeanAllenGHPractice/Utilities/ErrorMessage.swift
+++ SeanAllenGHPractice/Utilities/ErrorMessage.swift
@@ -9,7 +9,9 @@
 import Foundation
 
 // raw value is the type that the enum conforms to.
-enum ErrorMessage: String {
+// it will have a raw type String and conform to the Error protocol implementing now Apple's Value type
+// renamed to GFError
+enum GFError: String, Error {
 
     case invalidUserName = "This username created an invalid request"
     case unableToComplete = "Unable to complete, check your internet connection"
