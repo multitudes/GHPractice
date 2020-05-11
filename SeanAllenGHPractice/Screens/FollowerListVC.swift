e3e0a20efe0d4524b93d974f9aa601673485f225
laurent <wrmultitudes@gmail.com>
2020-05-11 16:07:58 +0200

    did the loading view animated - ready for empty state view

--- SeanAllenGHPractice/Screens/FollowerListVC.swift
+++ SeanAllenGHPractice/Screens/FollowerListVC.swift
@@ -52,9 +52,14 @@ class FollowerListVC: UIViewController {
     
     
     func getFollowers(username: String, page: Int) {
+        
+        showLoadingView()
+        
         NetworkManager.shared.getFollowers(for: username, page: page) { [weak self] result in
-            
+                        
             guard let self = self else { return } // without this line I would need to unwrap all self? manually
+            
+            self.dismissLoadingView()
             
             switch result {
                 case .success(let followers):
