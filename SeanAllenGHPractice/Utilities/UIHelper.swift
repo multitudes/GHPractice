890bb5f0984027c644560a9185434ee29fb5290c
laurent <wrmultitudes@gmail.com>
2020-05-11 14:46:58 +0200

    did some ARC checks with weak self in network call functions - ready for dl and caching avatar images

---
+++ SeanAllenGHPractice/Utilities/UIHelper.swift
@@ -0,0 +1,26 @@
+//
+//  UIHelper.swift
+//  SeanAllenGHPractice
+//
+//  Created by Laurent B on 11/05/2020.
+//  Copyright © 2020 Laurent B. All rights reserved.
+//
+
+import UIKit
+
+
+struct UIHelper {
+
+    static func createThreeColumnFlowLayout(in view: UIView) -> UICollectionViewFlowLayout {
+    let width = view.bounds.width
+    let padding: CGFloat = 12
+    let minimumItemSpacing: CGFloat = 10
+    let avalaibleWidth = width - ( padding * 2 ) - (minimumItemSpacing * 2)
+    let itemWidth = avalaibleWidth / 3
+    
+    let flowLayout = UICollectionViewFlowLayout()
+    flowLayout.sectionInset = UIEdgeInsets(top: padding, left: padding, bottom: padding, right: padding)
+    flowLayout.itemSize = CGSize(width: itemWidth, height: itemWidth + 40)
+    
+    return flowLayout
+}}
