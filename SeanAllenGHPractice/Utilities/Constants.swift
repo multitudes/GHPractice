0dc3b17cb116c2947112b914d6208ec40a069044
laurent <wrmultitudes@gmail.com>
2020-05-12 15:02:46 +0200

    item info view done = go for superclass!

--- SeanAllenGHPractice/Utilities/Constants.swift
+++ SeanAllenGHPractice/Utilities/Constants.swift
@@ -13,4 +13,8 @@ enum SFSymbols {
     
     static let location = "mappin.and.ellipse"
     static let noLocation = ""
+    static let repos = "folder"
+    static let gists = "text.alignleft"
+    static let followers = "heart"
+    static let following = "person.2"
 }
