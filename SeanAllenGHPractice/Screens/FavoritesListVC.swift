54268cfbc7dc7c2eb0f0dd2b3818bf40a41f59ea
laurent <wrmultitudes@gmail.com>
2020-05-13 17:36:02 +0200

    persistence done in default - going to tableviews

--- SeanAllenGHPractice/Screens/FavoritesListVC.swift
+++ SeanAllenGHPractice/Screens/FavoritesListVC.swift
@@ -13,6 +13,16 @@ class FavoritesListVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBlue
+        
+        PersistenceManager.retrieveFavorite { (result) in
+            switch result {
+                case .success(let favorites):
+                print(favorites)
+                    break
+                case .failure(let error):
+                    print(error.rawValue)
+            }
+        }
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -20,14 +30,6 @@ class FavoritesListVC: UIViewController {
         navigationController?.setNavigationBarHidden(false, animated: true)
     }
     
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+   
 
 }
