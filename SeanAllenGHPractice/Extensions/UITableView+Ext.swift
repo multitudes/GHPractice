0811e3e74848b1da5c96711f09a83349d667dd2f
laurent <wrmultitudes@gmail.com>
2020-05-14 17:44:50 +0200

    done for persistence - go for some more iPhoneSE

---
+++ SeanAllenGHPractice/Extensions/UITableView+Ext.swift
@@ -0,0 +1,27 @@
+//
+//  UITableView+Ext.swift
+//  SeanAllenGHPractice
+//
+//  Created by Laurent B on 14/05/2020.
+//  Copyright © 2020 Laurent B. All rights reserved.
+//
+
+import UIKit
+
+extension UITableView {
+    
+    func reloadDataOnMainThread() {
+        DispatchQueue.main.async {
+            self.reloadData()
+        }
+    }
+    
+    
+    func removeExcessCells() {
+        tableFooterView = UIView(frame: .zero
+        )
+    }
+    
+    
+    
+}
